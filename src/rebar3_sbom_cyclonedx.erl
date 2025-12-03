cedadad9163163189fa45a2f08c964eaf88d6fcd
Gwendal Laurent <44499033+GwendalLaurent@users.noreply.github.com>
2025-12-03 17:05:47 +0100

    fix parsing error (#26)

--- src/rebar3_sbom_cyclonedx.erl
+++ src/rebar3_sbom_cyclonedx.erl
@@ -38,7 +38,7 @@ bom({FilePath, _} = FileInfo, IsStrictVersion, App, Plugin, Serial, MetadataInfo
         SBoM#sbom{version = V}
     catch
         _:Reason:_Stacktrace ->
-            logger:error(
+            rebar_api:error(
                 "scan file:~ts failed, reason:~p, will use the default version number ~p",
                 [FilePath, Reason, ?DEFAULT_VERSION]
             ),
@@ -246,7 +246,7 @@ version(IsStrictVersion, {_, OldSBoM}) when not (IsStrictVersion) ->
         "Incrementing the SBoM version unconditionally: strict_version is set to false.", []
     ),
     OldSBoM#sbom.version + 1;
-version(IsStrictVersion, {NewSBoM, OldSBoM}) when not (IsStrictVersion) ->
+version(IsStrictVersion, {NewSBoM, OldSBoM}) when IsStrictVersion ->
     case is_sbom_equal(NewSBoM, OldSBoM) of
         true ->
             rebar_api:info(
